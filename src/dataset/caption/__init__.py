3eee7441649cb7c3d7f503a3027524a70518e9aa
Plat <60182057+p1atdev@users.noreply.github.com>
2025-01-17 12:40:22 +0900

    fix: add caption drop processor

--- src/dataset/caption/__init__.py
+++ src/dataset/caption/__init__.py
@@ -1,5 +1,8 @@
 from .util import CaptionProcessorMixin, CaptionPassthrough
 from .shuffle import CaptionShuffle
 from .prefix import CaptionPrefix
+from .drop import CaptionDrop
 
-CaptionProcessorList = list[CaptionPassthrough | CaptionPrefix | CaptionShuffle]
+CaptionProcessorList = list[
+    CaptionPassthrough | CaptionPrefix | CaptionShuffle | CaptionDrop
+]
