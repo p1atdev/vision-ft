d093ce8ed0bad4d725d5604cf89f98487d06e3aa
Plat <60182057+p1atdev@users.noreply.github.com>
2025-03-02 15:14:20 +0900

    feat: image patcher

---
+++ src/modules/patch.py
@@ -0,0 +1,140 @@
+import torch
+import torch.nn as nn
+
+
+def patchify(image: torch.Tensor, patch_size: int) -> torch.Tensor:
+    """
+    Converts an image tensor into patches.
+
+    Args:
+        image: Input image tensor with shape (batch_size, channels, height, width)
+        patch_size: Size of each patch
+
+    Returns:
+        Tensor of patches with shape (batch_size, num_vertical_patches*num_horizontal_patches, patch_size*patch_size*channels)
+    """
+    batch_size, channels, height, width = image.shape
+
+    # Reshape image into patches
+    patches = image.view(
+        batch_size,
+        channels,
+        height // patch_size,
+        patch_size,
+        width // patch_size,
+        patch_size,
+    )
+
+    # Rearrange dimensions and flatten patches
+    patches = patches.permute(0, 2, 4, 1, 3, 5)  # [B, H, W, C, P_H, P_W]
+    patches = patches.reshape(
+        batch_size,
+        (height // patch_size) * (width // patch_size),  # Merge height and width
+        patch_size * patch_size * channels,  # Merge channels and patch dims
+    )
+
+    return patches
+
+
+def unpatchify(
+    patches: torch.Tensor,
+    latent_height: int,
+    latent_width: int,
+    patch_size: int,
+    out_channels: int,
+) -> torch.Tensor:
+    """
+    Reconstructs the original image from a tensor of patches.
+
+    Args:
+        patches: Tensor of patches with shape (batch_size, latent_height*latent_width, patch_size*patch_size*channels)
+        height: Number of patches in the vertical direction
+        width: Number of patches in the horizontal direction
+        patch_size: Size of each patch
+        out_channels: Number of channels in the output image
+
+    Returns:
+        Reconstructed image tensor with shape (batch_size, channels, height*patch_size, width*patch_size)
+    """
+    batch_size, _num_patches, pxpxc = patches.shape
+
+    # Reshape patches into spatial dimensions
+    patches = patches.reshape(
+        batch_size,
+        latent_height,
+        latent_width,
+        out_channels,
+        patch_size,
+        patch_size,
+    )
+
+    # Rearrange dimensions to reconstruct image
+    # From: [batch, h, w, channels, patch_h, patch_w]
+    # To: [batch, channels, height*patch_h, width*patch_w]
+    patches = torch.einsum("nhwcpq->nchpwq", patches)
+    output = patches.reshape(
+        batch_size,
+        out_channels,
+        latent_height * patch_size,
+        latent_width * patch_size,
+    )
+    return output
+
+
+class ImagePatcher(nn.Module):
+    """
+    Module to convert an image tensor into patches.
+    """
+
+    def __init__(self, patch_size: int, out_channels: int):
+        super().__init__()
+
+        self.patch_size = patch_size
+        self.out_channels = out_channels
+
+    def patchify(self, image: torch.Tensor) -> torch.Tensor:
+        """
+        Converts an image tensor into patches.
+
+        Args:
+            image: Input image tensor with shape (batch_size, channels, height, width)
+
+        Returns:
+            Tensor of patches with shape (batch_size, num_vertical_patches*num_horizontal_patches, patch_size*patch_size*channels)
+        """
+
+        return patchify(image, self.patch_size)
+
+    def unpatchify(
+        self,
+        patches: torch.Tensor,
+        latent_height: int,
+        latent_width: int,
+    ) -> torch.Tensor:
+        """
+        Reconstructs the original image from a tensor of patches.
+
+        Args:
+            patches: Tensor of patches with shape (batch_size, latent_height*latent_width, patch_size*patch_size*channels)
+            latent_height: Number of patches in the vertical direction
+            latent_width: Number of patches in the horizontal direction
+
+        Returns:
+            Reconstructed image tensor with shape (batch_size, channels, height*patch_size, width*patch_size)
+
+        """
+
+        # patcher does not know the original image size,
+        # and can't infer it from the patches alone if it is not a square image.
+        # so, we need to pass the latent height and width to unpatchify
+
+        return unpatchify(
+            patches,
+            latent_height,
+            latent_width,
+            self.patch_size,
+            self.out_channels,
+        )
+
+    def forward(self, image: torch.Tensor) -> torch.Tensor:
+        return self.patchify(image)
---
+++ tests/test_patch.py
@@ -0,0 +1,80 @@
+from typing import NamedTuple
+
+import torch
+
+from src.modules.patch import patchify, unpatchify, ImagePatcher
+
+
+class TestCase(NamedTuple):
+    channels: int
+    patch_size: int
+    height: int
+    width: int
+    batch_size: int
+
+
+@torch.inference_mode()
+def test_auto_patchify_image():
+    test_cases = [
+        TestCase(channels=3, patch_size=4, height=256, width=256, batch_size=1),
+        TestCase(channels=16, patch_size=2, height=832, width=1152, batch_size=4),
+    ]
+
+    for test_case in test_cases:
+        channels, patch_size, height, width, batch_size = test_case
+
+        image = torch.rand(batch_size, channels, height, width)
+
+        patches = patchify(image, patch_size)
+        assert patches.shape == (
+            batch_size,
+            (height // patch_size) * (width // patch_size),
+            patch_size * patch_size * channels,
+        )
+
+        latent_height, latent_width = height // patch_size, width // patch_size
+
+        reconstruction = unpatchify(
+            patches, latent_height, latent_width, patch_size, channels
+        )
+        assert reconstruction.shape == (
+            batch_size,
+            channels,
+            height,
+            width,
+        )
+
+        assert torch.equal(image, reconstruction)
+
+
+@torch.inference_mode()
+def test_image_patcher():
+    test_cases = [
+        TestCase(channels=3, patch_size=4, height=256, width=256, batch_size=1),
+        TestCase(channels=16, patch_size=2, height=832, width=1152, batch_size=4),
+    ]
+
+    for test_case in test_cases:
+        channels, patch_size, height, width, batch_size = test_case
+
+        image = torch.rand(batch_size, channels, height, width)
+
+        patcher = ImagePatcher(patch_size=patch_size, out_channels=channels)
+        patches = patcher.patchify(image)
+        assert patches.shape == (
+            batch_size,
+            (height // patch_size) * (width // patch_size),
+            patch_size * patch_size * channels,
+        )
+
+        latent_height, latent_width = height // patch_size, width // patch_size
+
+        reconstruction = patcher.unpatchify(patches, latent_height, latent_width)
+        assert reconstruction.shape == (
+            batch_size,
+            channels,
+            height,
+            width,
+        )
+
+        assert torch.equal(image, reconstruction)
