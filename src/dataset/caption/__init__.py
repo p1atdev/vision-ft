196b5840e13e03e7a8af06932e06826406da9079
Plat <60182057+p1atdev@users.noreply.github.com>
2025-04-09 22:05:48 +0900

    feat: tag drop processor

--- src/dataset/caption/__init__.py
+++ src/dataset/caption/__init__.py
@@ -1,8 +1,8 @@
 from .util import CaptionProcessorMixin, CaptionPassthrough
 from .shuffle import CaptionShuffle
 from .prefix import CaptionPrefix
-from .drop import CaptionDrop
+from .drop import CaptionDrop, CaptionTagDrop
 
 CaptionProcessorList = list[
-    CaptionPassthrough | CaptionPrefix | CaptionShuffle | CaptionDrop
+    CaptionPassthrough | CaptionPrefix | CaptionShuffle | CaptionDrop | CaptionTagDrop
 ]
--- src/dataset/caption/drop.py
+++ src/dataset/caption/drop.py
@@ -15,3 +15,16 @@ class CaptionDrop(CaptionProcessorMixin):
             return ""
 
         return caption
+
+
+class CaptionTagDrop(CaptionProcessorMixin):
+    type: Literal["tag_drop"] = "tag_drop"
+
+    drop_rate: float
+    separator: str = ","
+
+    def process(self, caption: str) -> str:
+        tags = caption.split(self.separator)
+
+        tags = [tag for tag in tags if random.random() >= self.drop_rate]
+        return self.separator.join(tags)
