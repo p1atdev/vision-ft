550db416299e84c53164f064eb9c07e963b59ba5
Plat <60182057+p1atdev@users.noreply.github.com>
2025-06-19 00:32:55 +0900

    feat: self reference and another reference

---
+++ scripts/train/sdxl/prompt_free.self.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source .venv/bin/activate
+
+accelerate launch train/sdxl/prompt_free.self.py $@
--- scripts/train/sdxl/prompt_free.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source .venv/bin/activate
-
-accelerate launch train/sdxl/prompt_free.py $@
