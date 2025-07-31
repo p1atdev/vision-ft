9f73986d73675f04670487163b21f0f4337925e0
Plat <60182057+p1atdev@users.noreply.github.com>
2025-07-31 12:34:41 +0900

    wip: lumina image 2.0

---
+++ src/models/lumina2/config.py
@@ -0,0 +1,39 @@
+from pydantic import BaseModel
+
+import torch
+
+from ...utils.dtype import str_to_dtype
+
+
+#  NextDiT_2B_GQA_patch2_Adaln_Refiner
+class DenoiserConfig(BaseModel):
+    in_channels: int = 16
+    out_channels: int = 16
+    vec_in_dim: int = 768
+    context_in_dim: int = 4096
+    hidden_size: int = 2304
+    mlp_ratio: float = 4.0
+
+    depth: int = 26  # DiT blocks
+    num_heads: int = 24
+    num_kv_heads: int = 8
+    refiner_depth: int = 2  # Refiner blocks
+
+    axes_dim: list[int] = [32, 32, 32]  # for rope
+    aces_lens: list[int] = [300, 512, 512]  # for rope
+    theta: int = 10_000  # for rope
+    qkv_bias: bool = True
+
+    patch_size: int = 2
+    vae_channels: int = 16
+
+
+class LuminaImage2Config(BaseModel):
+    checkpoint_path: str
+
+    dtype: str = "bfloat16"
+
+    denoiser: DenoiserConfig = DenoiserConfig()
+
+    def get_dtype(self) -> torch.dtype:
+        return str_to_dtype(self.dtype)
